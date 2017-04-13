22ab783cabebff9542a5e72cc97cae2afe8b58a3
codemattcode <codemattcode@gmail.com>
2017-04-13 15:40:48 +0100

    cl

--- server.js
+++ server.js
@@ -2,15 +2,18 @@
 
 const express = require('express');
 const app = express();
-const bodyParser = require('bodyParser'); //req.body
+const bodyParser = require('body-parser'); //req.body
 const cors = require('cors'); //allow req from external api's/domains :8080
+
+let contacts = require('./data');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 
 
 app.get('/api/contacts', (req, res)=>{
-  res.send('Hello, World!');
+  // res.send('Hello, World!');
+  res.json(contacts);
 });
 //http Verbs : GET, POST, PUT, DELETE, PATCH.  "Methods used to send requests to server".
 
