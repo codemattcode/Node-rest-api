4117070dec8befd9583833e109f53f73aca5f8f7
codemattcode <codemattcode@gmail.com>
2017-04-13 15:08:20 +0100

    cl

--- server.js
+++ server.js
@@ -7,6 +7,13 @@ const cors = require('cors'); //allow req from external api's/domains :8080
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
+
+
+app.get('/api/contacts', (req, res)=>{
+  res.send('Hello, World!');
+});
+//http Verbs : GET, POST, PUT, DELETE, PATCH.  "Methods used to send requests to server".
+
 const hostname = 'localhost';
 const port = 5555;
 
